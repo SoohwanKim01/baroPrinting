c567465e963e96af11de94aa867ee2b627930b66
baro <scstnghks@naver.com>
2019-10-21 12:05:31 +0900

    추가

--- user/admin.php
+++ user/admin.php
@@ -7,11 +7,10 @@ if (isset($_SESSION['uno'])) {  // 로그인 상태면
     } else {
         $current_menu = '정보변경' ;
     }        
-} else {                        // 로그인이 안되었으면 
+} else { // 로그인이 안되었으면
     $current_menu = '로그인' ;
 }    
 $side_contents = require_once(SKIN_DIR .'/menu_sub.php') ;
-require_once(SKIN_DIR .'/layout2.php'); 
-echo '숙제! 관리자 페이지는 여분이 직접 완성해 보세요.' ;
+require_once(SKIN_DIR .'/layout2.php');
 require_once(SKIN_DIR .'/footer.php'); 
 ?>
